0dcb888e509c9fd36f2061e13b5e4135abb36320
Nikola-ra <129279814+Nikola-ra@users.noreply.github.com>
2025-01-17 14:19:03 +0100

    Autenticazione

--- MealMate/app/_layout.tsx
+++ MealMate/app/_layout.tsx
@@ -1,5 +1,24 @@
-import { Stack } from "expo-router";
+import { tokenCache } from "@/lib/cache"
+import { ClerkProvider, ClerkLoaded } from "@clerk/clerk-expo"
+import { Stack } from "expo-router/stack"
+import React from "react"
 
-export default function RootLayout() {
-  return <Stack.Screen name="index"/>;
+export default function Layout() {
+  const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
+
+  if (!publishableKey) {
+    throw new Error(
+      "Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env"
+    )
+  }
+
+  return (
+    <ClerkProvider tokenCache={tokenCache} publishableKey={publishableKey}>
+      <ClerkLoaded>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        </Stack>
+      </ClerkLoaded>
+    </ClerkProvider>
+  )
 }

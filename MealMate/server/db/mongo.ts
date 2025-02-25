b97bee0262d30291bc9535f09aeeaa6a8705e321
Nikola-ra <129279814+Nikola-ra@users.noreply.github.com>
2025-02-25 19:58:56 +0100

    Finire connessione

---
+++ MealMate/server/db/mongo.ts
@@ -0,0 +1,29 @@
+import mongoose from "mongoose"
+
+const MONGO_URI =
+  process.env.NODE_ENV === "production"
+    ? process.env.MONGO_URI_PROD
+    : process.env.MONGO_URI_DEV
+
+if (!MONGO_URI) {
+  throw new Error("MongoDB URI is missing!")
+}
+
+export async function connectDB() {
+  if (mongoose.connection.readyState >= 1) {
+    console.log("Already connected to MongoDB.")
+    return mongoose
+  }
+
+  try {
+    await mongoose.connect(MONGO_URI as string, {
+      dbName: process.env.NODE_ENV === "production" ? "production" : "dev",
+    })
+    console.log(`Connected to MongoDB (${process.env.NODE_ENV})`)
+  } catch (error) {
+    console.error("MongoDB Connection Error:", error)
+    throw error
+  }
+
+  return mongoose
+}
